47ca52656724ee201f168e3a81434dabc32dd56c
Lindsay Esterman <lindsayesterman@gmail.com>
2020-08-23 08:57:05 -0400

    authorization

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ console.log(process.env.API_TOKEN)
 const app = express()
 
 app.use(morgan('dev'))
+app.use(function validateBearerToken(req, res, next) {
+  const authToken = req.get('Authorization')
+  const apiToken = process.env.API_TOKEN
+  console.log('validate bearer token middleware')
+  console.log(apiToken);
+  console.log(authToken)
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  next()
+})
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
 function handleGetTypes(req, res){
