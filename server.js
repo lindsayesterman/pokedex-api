4827194b7a878ea38518ac85a5ef979558533b86
Lindsay Esterman <lindsayesterman@gmail.com>
2020-08-23 11:19:15 -0400

    cors

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ const express = require('express')
 const morgan = require('morgan')
 require('dotenv').config()
 const POKEDEX = require('./pokedex.json')
+const cors = require('cors')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
+
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
   const authToken = req.get('Authorization')
+  console.log(authToken)
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
